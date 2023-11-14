a6bfcffa4b6766f4b2a40119b69996cb436d0279
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-11-13 19:16:48 -0800

    add a solutio to the LeetCode medium problems

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -381,6 +381,81 @@ console.log(canBeIncreasing([100, 21, 100])); // true
 console.log(canBeIncreasing([105, 924, 32, 968])); // true
 console.log(canBeIncreasing([512, 867, 904, 997, 403])); // true
 
+// * 11. Replace Elements With Greatest Element on Right Side ---------------------
+// Given an array arr, replace every element in that array
+// with the greatest element among the elements to its right,
+// and replace the last element with -1.
+// After doing so, return the array.
+
+// Example:
+// Input: arr = [17,18,5,4,6,1]
+// Output: [18,6,6,6,1,-1]
+// Explanation:
+// - index 0 --> the greatest element to the right of index 0 is index 1 (18).
+// - index 1 --> the greatest element to the right of index 1 is index 4 (6).
+// - index 2 --> the greatest element to the right of index 2 is index 4 (6).
+// - index 3 --> the greatest element to the right of index 3 is index 4 (6).
+// - index 4 --> the greatest element to the right of index 4 is index 5 (1).
+// - index 5 --> there are no elements to the right of index 5, so we put -1.
+
+var replaceElements = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (i === arr.length - 1) {
+      arr[i] = -1;
+    } else {
+      let max = Math.max.apply(null, arr.slice(i + 1));
+      arr[i] = max;
+    }
+  }
+  return arr;
+};
+
+console.log(replaceElements([17, 18, 5, 4, 6, 1])); // [18,6,6,6,1,-1]
+console.log(replaceElements([400])); // [-1]
+
+// * 12. Valid Mountain Array ---------------------
+// Given an array of integers arr,
+// return true if and only if it is a valid mountain array.
+// Recall that arr is a mountain array if and only if:
+// arr.length >= 3
+// There exists some i with 0 < i < arr.length - 1 such that:
+// arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+// arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+var validMountainArray = function (arr) {
+  if (arr.length < 3) return false;
+
+  let increasing = false;
+  let decreasing = false;
+
+  // If we decreasing before increasing, return false
+  // If we start from decreasing, return false
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < arr[i + 1]) {
+      // If decreasing before increasing
+      if (decreasing) {
+        return false;
+      }
+      increasing = true;
+    } else if (arr[i] > arr[i + 1]) {
+      decreasing = true;
+    } else if (arr[i] === arr[i + 1]) {
+      return false;
+    }
+  }
+
+  if ((decreasing && !increasing) || (increasing && !decreasing)) return false;
+  return true;
+};
+
+console.log(validMountainArray([2, 1])); // false (only decreasing numbers)
+console.log(validMountainArray([3, 5, 5])); // false (increasing, then plato)
+console.log(validMountainArray([0, 3, 2, 1])); // true (increasing, then decreasing)
+console.log(validMountainArray([2, 0, 2])); // false (decreasing, then increasing)
+console.log(validMountainArray([0, 1, 2, 4, 2, 1])); // true
+console.log(validMountainArray([0, 1, 2, 1, 2])); // false
+
 // ! =============== Simulation Problems ===================
 
 // * 1. Build Array From Permutation -----------------------------------------
--- Medium/Arrays/Arrays.js
+++ Medium/Arrays/Arrays.js
@@ -39,7 +39,7 @@ var wateringPlants = function (plants, capacity) {
   // Save the original capacity for refilling the can
   let originalCapacity = capacity;
 
-  // Set the necessary steps 
+  // Set the necessary steps
   let totalSteps = 0;
   let stepsToTheRiverAndBack = 0;
 
@@ -66,3 +66,44 @@ var wateringPlants = function (plants, capacity) {
 console.log(wateringPlants([2, 2, 3, 3], 5)); // 14
 console.log(wateringPlants([1, 1, 1, 4, 2, 3], 4)); // 30
 console.log(wateringPlants([7, 7, 7, 7, 7, 7, 7], 8)); // 49
+
+// * 2. Number of Times Binary String is Prefix-Aligned ---------------------
+// You have a 1-indexed binary string of length n where all the bits are 0 initially.
+// We will flip all the bits of this binary string
+// (i.e., change them from 0 to 1) one by one.
+// You are given a 1-indexed integer array flips
+// where flips[i] indicates that the bit at index i will be flipped in the ith step.
+
+// A binary string is prefix-aligned if, after the ith step,
+// all the bits in the inclusive range [1, i] are ones
+// and all the other bits are zeros.
+// Return the number of times the binary string is prefix-aligned during the flipping process.
+
+// Example:
+// Input: flips = [4,1,2,3]
+// Output: 1
+// Explanation: The binary string is initially "0000".
+// After applying step 1: The string becomes "0001", which is not prefix-aligned.
+// After applying step 2: The string becomes "1001", which is not prefix-aligned.
+// After applying step 3: The string becomes "1101", which is not prefix-aligned.
+// After applying step 4: The string becomes "1111", which is prefix-aligned.
+// We can see that the string was prefix-aligned 1 time, so we return 1.
+
+var numTimesAllBlue = function (flips) {
+  let end = 0;
+  let count = 0;
+  let result = 0;
+
+  for (let flip of flips) {
+    end = Math.max(end, flip); // based on the first test: (end = 0, flip = 3), (end = 3, flip = 2), (end = 3, flip = 4), (end = 4, flip = 1), (end = 4, flip = 5),
+    count += 1; // 1 // 2 // 3 // 4 // 5 (compare these numbers to the values of end above)
+    if (end !== count) continue;
+    // when end === count, increase result by 1
+    result += 1;
+  }
+  return result;
+};
+
+console.log(numTimesAllBlue([3, 2, 4, 1, 5])); // 2
+console.log(numTimesAllBlue([4, 1, 2, 3])); // 1
+console.log(numTimesAllBlue([1])); // 1
