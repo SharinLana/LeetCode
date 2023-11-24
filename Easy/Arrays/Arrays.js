0243de6b5589bedb3265c292f6923fe2ba5e001e
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-11-23 20:37:43 -0800

    add 1 solved problem: easy

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -1311,7 +1311,86 @@ console.log(findLengthOfLCIS([1, 3, 5, 7])); // 4
 console.log(findLengthOfLCIS([1, 3, 5, 4, 2, 3, 4])); // 3
 console.log(findLengthOfLCIS([1, 1, 2])); // 2
 
+// * 33. The Employee That Worked on the Longest Task ---------------------
+// There are n employees, each with a unique id from 0 to n - 1.
+// You are given a 2D integer array logs where logs[i] = [id[i], leaveTime[i]] where:
 
+// id[i] is the id of the employee that worked on the ith task, and
+// leaveTime[i] is the time at which the employee finished the ith task.
+// All the values leaveTime[i] are unique.
+// Note that the ith task starts the moment right after the (i - 1)th task ends,
+// and the 0th task starts at time 0.
+
+// Return the id of the employee that worked the task with the longest time.
+// If there is a tie between two or more employees, return the smallest id among them.
+
+// Example:
+// Input: n = 10, logs = [[0,3],[2,5],[0,9],[1,15]]
+// Output: 1
+// Explanation:
+// Task 0 started at 0 and ended at 3 with 3 units of times.
+// Task 1 started at 3 and ended at 5 with 2 units of times.
+// Task 2 started at 5 and ended at 9 with 4 units of times.
+// Task 3 started at 9 and ended at 15 with 6 units of times.
+// The task with the longest time is task 3 and the employee with id 1
+// is the one that worked on it, so we return 1.
+
+var hardestWorker = function (n, logs) {
+  let maxDiff = logs[0][1]; // store the number of units of time of the firs log
+  let employeeId = logs[0][0];
+
+  for (let i = 0; i < logs.length; i++) {
+    if (logs[i + 1]) {
+      let diff = Math.abs(logs[i][1] - logs[i + 1][1]);
+      if (maxDiff < diff) {
+        employeeId = logs[i + 1][0];
+        maxDiff = diff;
+      } else if (maxDiff === diff) {
+        employeeId = employeeId < logs[i+1][0] ? employeeId : logs[i+1][0];
+      } 
+    }
+  }
+
+  return employeeId;
+};
+
+console.log(
+  hardestWorker(10, [
+    [0, 3],
+    [2, 5],
+    [0, 9],
+    [1, 15],
+  ])
+); // 1
+console.log(
+  hardestWorker(26, [
+    [1, 1],
+    [3, 7],
+    [2, 12],
+    [7, 17],
+  ])
+); // 3
+console.log(
+  hardestWorker(2, [
+    [0, 10],
+    [1, 20],
+  ])
+); // 0 (Task 0 started at 0 and ended at 10 with 10 units of times.
+// Task 1 started at 10 and ended at 20 with 10 units of times.
+// The tasks with the longest time are tasks 0 and 1.
+//The employees that worked on them are 0 and 1, so we return the smallest id 0.)
+console.log(
+  hardestWorker(2, [
+    [114, 5],
+    [115, 7],
+    [50, 9],
+    [105, 11],
+    [18, 13],
+    [47, 16],
+    [48, 18],
+    [39, 19],
+  ])
+); // 114
 
 // ! =============== Simulation Problems ===================
 
