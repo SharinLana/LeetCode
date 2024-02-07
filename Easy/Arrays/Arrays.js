1a8452267e418724f2de82f451c973f08c24ebfd
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-02-06 19:38:47 -0800

    add 1 solved LeetCode problem

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -1876,6 +1876,45 @@ var differenceOfSum = function (nums) {
 console.log(differenceOfSum([1, 15, 6, 3])); // 9
 console.log(differenceOfSum([1, 2, 3, 4])); // 0
 
+// * 3. Sum of All Odd Length Subarrays -----------------------------------
+// Given an array of positive integers arr, return the sum of all possible odd-length subarrays of arr.
+// A subarray is a contiguous subsequence of the array.
+
+// Example:
+// Input: arr = [1,4,2,5,3]
+// Output: 58
+// Explanation: The odd-length subarrays of arr and their sums are:
+// [1] = 1
+// [4] = 4
+// [2] = 2
+// [5] = 5
+// [3] = 3
+// [1,4,2] = 7
+// [4,2,5] = 11
+// [2,5,3] = 10
+// [1,4,2,5,3] = 15
+// If we add all these together we get 1 + 4 + 2 + 5 + 3 + 7 + 11 + 10 + 15 = 58
+
+var sumOddLengthSubarrays = function (arr) {
+  let sum = 0;
+
+  // Traversing the array
+  for (let i = 0; i < arr.length; i++) {
+    // Defining every third element of the array
+    for (let j = i; j < arr.length; j += 2) {
+      // Traversing the subarray (each element from i to j)
+      for (let k = i; k <= j; k++) {
+        sum += arr[k];
+      }
+    }
+  }
+  return sum;
+};
+
+console.log(sumOddLengthSubarrays([1, 4, 2, 5, 3])); // 58
+console.log(sumOddLengthSubarrays([1, 2])); // 3
+console.log(sumOddLengthSubarrays([10, 11, 12])); // 66
+
 // ! =============== Array of Strings ===================
 
 // * 1. Find Words Containing Character ----------------------------------------------------
