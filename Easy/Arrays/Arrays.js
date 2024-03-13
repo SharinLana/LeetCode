1a2d6d8dcb4fb47946927472d9fccdcdcff57495
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-03-12 19:10:37 -0700

    add 1 solved LeetCode problem

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -1962,6 +1962,65 @@ const calculateMin = (arr) => {
 console.log(maxProductDifference([5, 6, 2, 7, 4])); // 34
 console.log(maxProductDifference([4, 2, 5, 9, 7, 4, 8])); // 64
 
+// * 5. Minimum Time Visiting All points -----------------------------------
+// On a 2D plane, there are n points with integer coordinates points[i] = [xi, yi]. Return the minimum time in seconds to visit all the points in the order given by points.
+
+// You can move according to these rules:
+
+// In 1 second, you can either:
+// move vertically by one unit,
+// move horizontally by one unit, or
+// move diagonally sqrt(2) units (in other words, move one unit vertically then one unit horizontally in 1 second).
+// You have to visit the points in the same order as they appear in the array.
+// You are allowed to pass through points that appear later in the order, but these do not count as visits.
+
+// Example 1:
+// Input: points = [[1,1],[3,4],[-1,0]]
+// Output: 7
+// Explanation: One optimal path is [1,1] -> [2,2] -> [3,3] -> [3,4] -> [2,3] -> [1,2] -> [0,1] -> [-1,0]
+// Time from [1,1] to [3,4] = 3 seconds
+// Time from [3,4] to [-1,0] = 4 seconds
+// Total time = 7 seconds
+
+// Example 2:
+
+// Input: points = [[3,2],[-2,2]]
+// Output: 5
+
+var minTimeToVisitAllPoints = function (points) {
+  let sum = 0;
+
+  for (let i = 0; i < points.length; i++) {
+    if (points[i+1] !== undefined ) {
+      let diffX = Math.abs(points[i][0] - points[i+1][0]);
+      let diffY = Math.abs(points[i][1] - points[i+1][1]);
+
+      if (diffX >= diffY) {
+        sum += diffX;
+      } else {
+        sum += diffY;
+      }
+    }
+  }
+  return sum;
+};
+
+console.log(
+  "minTimeToVisitAllPoints: " +
+    minTimeToVisitAllPoints([
+      [1, 1],
+      [3, 4],
+      [-1, 0],
+    ])
+); // 7
+console.log(
+  "minTimeToVisitAllPoints: " +
+    minTimeToVisitAllPoints([
+      [3, 2],
+      [-2, 2],
+    ])
+); // 5
+
 // ! =============== Array of Strings ===================
 
 // * 1. Find Words Containing Character ----------------------------------------------------
