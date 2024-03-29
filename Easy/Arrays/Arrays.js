58ae4ee9add3571e263465ddbc5d5e55ceea00af
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-03-29 15:05:06 -0700

    add 1 solved LeetCode problem

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -1991,9 +1991,9 @@ var minTimeToVisitAllPoints = function (points) {
   let sum = 0;
 
   for (let i = 0; i < points.length; i++) {
-    if (points[i+1] !== undefined ) {
-      let diffX = Math.abs(points[i][0] - points[i+1][0]);
-      let diffY = Math.abs(points[i][1] - points[i+1][1]);
+    if (points[i + 1] !== undefined) {
+      let diffX = Math.abs(points[i][0] - points[i + 1][0]);
+      let diffY = Math.abs(points[i][1] - points[i + 1][1]);
 
       if (diffX >= diffY) {
         sum += diffX;
@@ -2515,6 +2515,44 @@ var sumCounts = function (nums) {
 
 console.log(sumCounts([1, 2, 1])); // 15
 console.log(sumCounts([1, 1])); // 3
+
+// * 5. Find the Distinct Difference Array ------------------------------------------
+// You are given a 0-indexed array nums of length n.
+// The distinct difference array of nums is an array diff of length n
+// such that diff[i] is equal to the number of distinct elements
+// in the suffix nums[i + 1, ..., n - 1] subtracted
+// from the number of distinct elements in the prefix nums[0, ..., i].
+// Return the distinct difference array of nums.
+// Note that nums[i, ..., j] denotes the subarray of nums starting at index i
+// and ending at index j inclusive. Particularly, if i > j then nums[i, ..., j]
+// denotes an empty subarray.
+
+// Example:
+// Input: nums = [1,2,3,4,5]
+// Output: [-3,-1,1,3,5]
+// Explanation: For index i = 0, there is 1 element in the prefix and 4 distinct elements in the suffix. Thus, diff[0] = 1 - 4 = -3.
+// For index i = 1, there are 2 distinct elements in the prefix and 3 distinct elements in the suffix. Thus, diff[1] = 2 - 3 = -1.
+// For index i = 2, there are 3 distinct elements in the prefix and 2 distinct elements in the suffix. Thus, diff[2] = 3 - 2 = 1.
+// For index i = 3, there are 4 distinct elements in the prefix and 1 distinct element in the suffix. Thus, diff[3] = 4 - 1 = 3.
+// For index i = 4, there are 5 distinct elements in the prefix and no elements in the suffix. Thus, diff[4] = 5 - 0 = 5.
+
+var distinctDifferenceArray = function (nums) {
+  let output = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    output[i] =
+      new Set(nums.slice(0, i + 1)).size - new Set(nums.slice(i + 1)).size;
+  }
+
+  return output;
+};
+
+console.log(
+  "distinctDifferenceArray" + distinctDifferenceArray([1, 2, 3, 4, 5])
+); // [-3,-1,1,3,5]
+console.log(
+  "distinctDifferenceArray" + distinctDifferenceArray([3, 2, 3, 4, 2])
+); // [-2,-1,0,2,3]
 
 // ! =================== Matrix ====================
 
