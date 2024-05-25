a566ba2a56f2bd1dc9dd481c7a4a6d9028055fbe
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-05-25 11:38:13 -0700

    solce a LeetCode problem #2980. Check if Bitwise OR Has Trailing Zeros

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -3233,6 +3233,43 @@ console.log(singleNumber([2, 2, 1])); // 1
 console.log(singleNumber([4, 1, 2, 1, 2])); // 4
 console.log(singleNumber([1])); // 1
 
+// * 4. Check if Bitwise OR Has Trailing Zeros -----------------------------------
+// You are given an array of positive integers nums.
+// You have to check if it is possible to select two or more elements in the array
+// such that the bitwise OR of the selected elements 
+// has at least one trailing zero in its binary representation.
+
+// For example, the binary representation of 5, 
+// which is "101", does not have any trailing zeros, 
+// whereas the binary representation of 4, which is "100", 
+// has two trailing zeros.
+
+// Return true if it is possible to select two or more elements 
+// whose bitwise OR has trailing zeros, return false otherwise.
+
+// Example:
+
+// Input: nums = [1,2,3,4,5]
+// Output: true
+// Explanation: If we select the elements 2 and 4, their bitwise OR is 6, 
+// which has the binary representation "110" with one trailing zero.
+
+const hasTrailingZeros = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i+1; j < nums.length; j++) {
+      let binaryOR = (nums[i] | nums[j]).toString(2);
+
+      if (binaryOR[binaryOR.length - 1] === "0") return true;
+    }
+  }
+
+  return false;
+};
+
+console.log(hasTrailingZeros([1, 2, 3, 4, 5])); // true
+console.log(hasTrailingZeros([2, 4, 8, 16])); // true
+console.log(hasTrailingZeros([1, 3, 5, 7, 9])); // false
+
 // ! =============== Greedy Algorithm ==============
 
 // ! =============== Backtracking ==============
