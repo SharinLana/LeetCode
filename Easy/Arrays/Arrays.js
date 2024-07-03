f9129acc536f26a6cd3234052cd8433f30f34861
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-07-02 21:49:34 -0700

    solve a LeetCode problem #999 Available Captures for Rook

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -3043,6 +3043,149 @@ console.log(
   ])
 ); // false
 
+// * 4. (999). Available Captures for Rook ------------------------------------------
+// On an 8 x 8 chessboard, there is exactly one white rook 'R' and some number of white bishops 'B',
+// black pawns 'p', and empty squares '.'.
+// When the rook moves, it chooses one of four cardinal directions
+// (north, east, south, or west),
+// then moves in that direction until it chooses to stop,
+// reaches the edge of the board, captures a black pawn, or is blocked by a white bishop.
+// A rook is considered attacking a pawn if the rook can capture the pawn on the rook's turn.
+// The number of available captures for the white rook is the number of pawns
+// that the rook is attacking.
+// Return the number of available captures for the white rook.
+
+// Example 1:
+// Input: board = [
+// [".",".",".",".",".",".",".","."],
+// [".",".",".","p",".",".",".","."],
+// [".",".",".","R",".",".",".","p"],
+// [".",".",".",".",".",".",".","."],
+// [".",".",".",".",".",".",".","."],
+// [".",".",".","p",".",".",".","."],
+// [".",".",".",".",".",".",".","."],
+// [".",".",".",".",".",".",".","."]]
+// Output: 3
+// Explanation: In this example, the rook is attacking all the pawns.
+
+// Example 2:
+// Input: board = [
+// [".",".",".",".",".",".",".","."],
+// [".","p","p","p","p","p",".","."],
+// [".","p","p","B","p","p",".","."],
+// [".","p","B","R","B","p",".","."],
+// [".","p","p","B","p","p",".","."],
+// [".","p","p","p","p","p",".","."],
+// [".",".",".",".",".",".",".","."],
+// [".",".",".",".",".",".",".","."]]
+// Output: 0
+// Explanation: The bishops are blocking the rook from attacking any of the pawns.
+
+let numRookCaptures = function (board) {
+  let count = 0;
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      if (board[i][j] == "R") {
+        count += upCheck(i, j, board) ? 1 : 0;
+        count += downCheck(i, j, board) ? 1 : 0;
+        count += rightCheck(i, j, board) ? 1 : 0;
+        count += leftCheck(i, j, board) ? 1 : 0;
+      }
+    }
+  }
+  return count;
+};
+
+let upCheck = function (m, n, board) {
+  for (let i = m; i >= 0; i--) {
+    if (board[i][n] == "p") {
+      return true;
+    } else if (board[i][n] == "B") {
+      return false;
+    }
+  }
+  return false;
+};
+let downCheck = function (m, n, board) {
+  for (let i = m; i < 8; i++) {
+    if (board[i][n] == "p") {
+      return true;
+    } else if (board[i][n] == "B") {
+      return false;
+    }
+  }
+  return false;
+};
+let leftCheck = function (m, n, board) {
+  for (let i = n; i >= 0; i--) {
+    if (board[m][i] == "p") {
+      return true;
+    } else if (board[m][i] == "B") {
+      return false;
+    }
+  }
+  return false;
+};
+let rightCheck = function (m, n, board) {
+  for (let i = n; i < 8; i++) {
+    if (board[m][i] == "p") {
+      return true;
+    } else if (board[m][i] == "B") {
+      return false;
+    }
+  }
+  return false;
+};
+
+console.log(
+  numRookCaptures([
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", "p", ".", ".", ".", "."],
+    [".", ".", ".", "R", ".", ".", ".", "p"],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", "p", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+  ])
+); // 3
+console.log(
+  numRookCaptures([
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", "p", "p", "p", "p", "p", ".", "."],
+    [".", "p", "p", "B", "p", "p", ".", "."],
+    [".", "p", "B", "R", "B", "p", ".", "."],
+    [".", "p", "p", "B", "p", "p", ".", "."],
+    [".", "p", "p", "p", "p", "p", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+  ])
+); // 0
+console.log(
+  numRookCaptures([
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", "p", ".", ".", ".", "."],
+    [".", ".", ".", "p", ".", ".", ".", "."],
+    ["p", "p", ".", "R", ".", "p", "B", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", "B", ".", ".", ".", "."],
+    [".", ".", ".", "p", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+  ])
+); // 3
+console.log(
+  numRookCaptures([
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", "p", ".", ".", ".", "."],
+    [".", ".", ".", "B", ".", ".", ".", "."],
+    [".", "p", "B", "R", "p", "B", "p", "."],
+    [".", ".", ".", "p", "p", ".", ".", "."],
+    [".", ".", ".", "B", ".", ".", ".", "."],
+    [".", ".", ".", "p", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+  ])
+); // 2
+
 // ! =============== Two Pointers ==============
 
 // * 1. Number of Arithmetic Triplets ------------------------------------------
