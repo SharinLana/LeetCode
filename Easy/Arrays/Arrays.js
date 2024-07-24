cf08fa5fb533d65cf4a26ab5d7b9fa46712b02a6
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-07-23 23:19:05 -0700

    solve a LeetCode problem #2506. Count Pairs Of Similar Strings

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -2428,6 +2428,49 @@ console.log(maximumValue(["alic3", "bob", "3", "4", "00000"])); // 5
 console.log(maximumValue(["1", "01", "001", "0001"])); // 1
 console.log(maximumValue(["3glko", "1"])); // 5
 
+// * . (2506) Count Pairs Of Similar Strings ------------------------------------------
+// You are given a 0-indexed string array words.
+// Two strings are similar if they consist of the same characters.
+// For example, "abca" and "cba" are similar since both consist of characters 
+// 'a', 'b', and 'c'.
+// However, "abacba" and "bcfd" are not similar since they do not consist 
+// of the same characters.
+// Return the number of pairs (i, j) such that 0 <= i < j <= word.length - 1 
+// and the two strings words[i] and words[j] are similar.
+
+
+// Example:
+// Input: words = ["aba","aabb","abcd","bac","aabc"]
+// Output: 2
+// Explanation: There are 2 pairs that satisfy the conditions:
+// - i = 0 and j = 1 : both words[0] and words[1] only consist of characters 'a' and 'b'. 
+// - i = 3 and j = 4 : both words[3] and words[4] only consist of characters 'a', 'b', and 'c'. 
+
+let similarPairs = function (words) {
+  let counter = 0;
+  for (let i = 0; i < words.length; i++) {
+    for (let j = i+1; j < words.length; j++) {
+      ifTheSame(words[i], words[j]) ? counter +=1 : counter;
+    }
+  }
+  return counter;
+};
+
+const ifTheSame = (word1, word2) => {
+  for (let char of word1) {
+    if (!word2.includes(char)) return false;
+  }
+  for (let char of word2) {
+    if (!word1.includes(char)) return false;
+  }
+  
+  return true;
+}
+
+console.log(similarPairs(["aba", "aabb", "abcd", "bac", "aabc"])); // 2
+console.log(similarPairs(["aabb", "ab", "ba"])); // 3
+console.log(similarPairs(["nba", "cba", "dba"])); // 0
+
 // ! =============== Enumeration ===================
 
 // ! =============== Hash Table ====================
