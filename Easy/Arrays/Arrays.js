f228efd398339e2582637d17f11c5a0e3824f84e
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-07-24 20:08:43 -0700

    solve a LeetCode problem #Distribute Candies

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -3046,6 +3046,50 @@ console.log(
   ])
 ); // []
 
+// * 12 (575). Distribute Candies ------------------------------------------
+// Alice has n candies, where the ith candy is of type candyType[i]. 
+// Alice noticed that she started to gain weight, so she visited a doctor.
+
+// The doctor advised Alice to only eat n / 2 of the candies she has 
+// (n is always even). Alice likes her candies very much, 
+// and she wants to eat the maximum number of different types of candies 
+// while still following the doctor's advice.
+
+// Given the integer array candyType of length n, 
+// return the maximum number of different types of candies she can eat 
+// if she only eats n / 2 of them.
+
+// Example 1:
+// Input: candyType = [1,1,2,2,3,3]
+// Output: 3
+// Explanation: Alice can only eat 6 / 2 = 3 candies. Since there are only 3 types, she can eat one of each type.
+
+// Example 2:
+// Input: candyType = [1,1,2,3]
+// Output: 2
+// Explanation: Alice can only eat 4 / 2 = 2 candies. 
+// Whether she eats types [1,2], [1,3], or [2,3], 
+// she still can only eat 2 different types.
+
+let distributeCandies = function (candyType) {
+  let uniqueTypes = new Set(candyType);
+  // let halfPortion = candyType.length / 2;
+  
+  if (candyType.length / 2 === uniqueTypes.size) {
+    return uniqueTypes.size;
+  } else {
+    if (candyType.length / 2 < uniqueTypes.size) {
+      return candyType.length / 2;
+    } else {
+      return uniqueTypes.size;
+    }
+  }
+};
+
+console.log(distributeCandies([1, 1, 2, 2, 3, 3])); // 3
+console.log(distributeCandies([1, 1, 2, 3])); // 2
+console.log(distributeCandies([6, 6, 6, 6])); // 1
+
 // ! =================== Matrix ====================
 
 // * 1. Matrix Cells in Distance Order ------------------------------------------
