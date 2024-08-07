2b930951815c2a5e3d2b82af56f8fd11ca7e2405
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-08-07 01:27:46 -0700

    solve a LeetCode problem #566. Reshape the Matrix

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -3679,6 +3679,69 @@ console.log(
   ])
 ); // [[1,4],[2,5],[3,6]]
 
+// * 7. (566). Reshape the Matrix ------------------------------------------
+// In MATLAB, there is a handy function called reshape 
+// which can reshape an m x n matrix into a new one with a different size r x c 
+// keeping its original data.
+
+// You are given an m x n matrix mat and two integers r and c representing 
+// the number of rows and the number of columns of the wanted reshaped matrix.
+
+// The reshaped matrix should be filled with all the elements of the original matrix 
+// in the same row-traversing order as they were.
+
+// If the reshape operation with given parameters is possible and legal, 
+// output the new reshaped matrix; Otherwise, output the original matrix.
+
+
+// Example 1:
+// Input: mat = [[1,2],[3,4]], r = 1, c = 4
+// Output: [[1,2,3,4]]
+
+// Example 2:
+// Input: mat = [[1,2],[3,4]], r = 2, c = 4
+// Output: [[1,2],[3,4]]
+
+let matrixReshape = function (mat, r, c) {
+  // Validate r and c:
+  let totalNumberOfCells = mat[0].length * mat.length;
+  if (r * c !== totalNumberOfCells) return mat;
+
+  let flattenedMat = mat.flat();
+  let result = Array.from({length: r}, () => Array.from({length: c}));
+
+  for (let i = 0; i < result.length; i++) {
+    for (let j = 0; j < result[i].length; j++) {
+      result[i][j] = flattenedMat.shift();
+    }
+  }
+
+  return result;
+};
+
+console.log(
+  matrixReshape([
+    [1, 2],
+    [3, 4],
+  ], 1, 4)
+); // [[1,2,3,4]]
+console.log(
+  matrixReshape([
+    [1, 2],
+    [3, 4],
+  ], 2, 4)
+); // [[1,2],[3,4]]
+console.log(
+  matrixReshape(
+    [
+      [1, 2, 3, 4, 5, 6],
+      [7, 8, 9, 10, 11, 12],
+    ],
+    3,
+    4
+  )
+);
+
 // ! =============== Two Pointers ==============
 
 // * 1. Number of Arithmetic Triplets ------------------------------------------
