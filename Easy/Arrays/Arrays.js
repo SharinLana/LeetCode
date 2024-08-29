cb115f4a334b039160b10aa918374e067ca7cd74
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-08-28 21:18:07 -0700

    solve a LeetCode Problem #976. Largest Perimeter Triangle

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -3896,13 +3896,16 @@ console.log(
 ); // true
 
 console.log(
-  areSimilar([
-    [4, 6],
-    [10, 1],
-    [8, 8],
-    [10, 9],
-    [9, 10],
-  ], 9)
+  areSimilar(
+    [
+      [4, 6],
+      [10, 1],
+      [8, 8],
+      [10, 9],
+      [9, 10],
+    ],
+    9
+  )
 ); // false
 
 // ! =============== Two Pointers ==============
@@ -4249,6 +4252,58 @@ console.log(hasTrailingZeros([2, 4, 8, 16])); // true
 console.log(hasTrailingZeros([1, 3, 5, 7, 9])); // false
 
 // ! =============== Greedy Algorithm ==============
+
+// * 1. (976). Largest Perimeter Triangle
+
+// Given an integer array nums,
+// return the largest perimeter of a triangle with a non-zero area,
+// formed from three of these lengths.
+// If it is impossible to form any triangle of a non-zero area,
+// return 0.
+// The sum of any two sides greater than third side
+
+// Example 1:
+// Input: nums = [2,1,2]
+// Output: 5
+// Explanation: You can form a triangle with three side lengths: 1, 2, and 2.
+
+// Example 2:
+// Input: nums = [1,2,1,10]
+// Output: 0
+// Explanation:
+// You cannot use the side lengths 1, 1, and 2 to form a triangle.
+// You cannot use the side lengths 1, 1, and 10 to form a triangle.
+// You cannot use the side lengths 1, 2, and 10 to form a triangle.
+// As we cannot use any three side lengths to form a triangle of non-zero area, we return 0.
+
+var largestPerimeter = function (nums, sorted = false) {
+  if (nums.length === 0) return 0;
+
+  if (nums.includes(0) || !sorted) {
+    nums = nums.filter((el) => el !== 0).sort((a, b) => a - b);
+    sorted = true;
+  }
+
+  let firstIdx = nums.length - 1;
+  let secondIdx = nums.length - 2;
+  let thirdIdx = nums.length - 3;
+
+  let firstSide = nums[firstIdx];
+  let secondSide = nums[secondIdx];
+  let thirdSide = nums[thirdIdx];
+
+  // validation
+  if (thirdSide + secondSide > firstSide) {
+    return firstSide + secondSide + thirdSide;
+  } else {
+    // remove the last elem
+    return largestPerimeter(nums.slice(0, nums.length - 1), sorted);
+  }
+};
+
+console.log(largestPerimeter([2, 1, 2])); // 5
+console.log(largestPerimeter([1, 2, 1, 10])); //  0
+console.log(largestPerimeter([3, 6, 2, 3])); // 8
 
 // ! =============== Backtracking ==============
 
