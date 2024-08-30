5d3b46dc7eb13b8960b61f769e38129a45233544
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-08-29 22:56:11 -0700

    solve a LeetCode problem #599. Minimum Index Sum of Two Lists

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -2663,6 +2663,80 @@ console.log(countPrefixSuffixPairs(["a", "aba", "ababa", "aa"])); // 4
 console.log(countPrefixSuffixPairs(["pa", "papa", "ma", "mama"])); // 2
 console.log(countPrefixSuffixPairs(["abab", "ab"])); // 0
 
+// * 12. (599). Minimum Index Sum of Two Lists  ------------------------------------------
+// Given two arrays of strings list1 and list2, find the common strings with the least index sum.
+// A common string is a string that appeared in both list1 and list2.
+// A common string with the least index sum is a common string such that
+// if it appeared at list1[i] and list2[j] then i + j should be the minimum value
+// among all the other common strings.
+// Return all the common strings with the least index sum. Return the answer in any order.
+
+// Example 1:
+// Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"]
+// Output: ["Shogun"]
+// Explanation: The only common string is "Shogun".
+
+// Example 2:
+// Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["KFC","Shogun","Burger King"]
+// Output: ["Shogun"]
+// Explanation: The common string with the least index sum is "Shogun" with index sum = (0 + 1) = 1.
+
+// Example 3:
+// Input: list1 = ["happy","sad","good"], list2 = ["sad","happy","good"]
+// Output: ["sad","happy"]
+// Explanation: There are three common strings:
+// "happy" with index sum = (0 + 1) = 1.
+// "sad" with index sum = (1 + 0) = 1.
+// "good" with index sum = (2 + 2) = 4.
+// The strings with the least index sum are "sad" and "happy".
+
+let findRestaurant = function (list1, list2) {
+  let result = [];
+  let shorterArray, longerArray;
+  let sumIdx = Infinity;
+
+  shorterArray = list1.length <= list2.length ? list1 : list2;
+  longerArray = list1.length > list2.length ? list1 : list2;
+
+  for (let i = 0; i < shorterArray.length; i++) {
+    if (longerArray.indexOf(shorterArray[i]) !== -1) {
+      let sum = i + longerArray.indexOf(shorterArray[i]);
+
+      if (sum < sumIdx) {
+        console.log("SUM: " + sum, "SumIdx: " + sumIdx);
+        sumIdx = sum;
+        result = [];
+        result.push(shorterArray[i]);
+      } else if (sum === sumIdx) {
+        sumIdx = sum;
+        result.push(shorterArray[i]);
+      }
+    }
+  }
+
+  return result;
+};
+
+console.log(
+  findRestaurant(
+    ["Shogun", "Tapioca Express", "Burger King", "KFC"],
+    [
+      "Piatti",
+      "The Grill at Torrey Pines",
+      "Hungry Hunter Steakhouse",
+      "Shogun",
+    ]
+  )
+); // ["Shogun"]
+console.log(
+  findRestaurant(
+    ["Shogun", "Tapioca Express", "Burger King", "KFC"],
+    ["KFC", "Shogun", "Burger King"]
+  )
+); // ["Shogun"]
+console.log(findRestaurant(["happy", "sad", "good"], ["sad", "happy", "good"])); // ["sad","happy"]
+console.log(findRestaurant(["S", "TEXP", "BK", "KFC"], ["KFC", "BK", "S"])); // ["S"]
+
 // ! =============== Enumeration ===================
 
 // ! =============== Hash Table ====================
