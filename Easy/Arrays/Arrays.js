90df08a08d2b61fa76ea3ce5acf0127178ed7401
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-09-06 15:02:03 -0700

    sove a LeetCode problem #350. Intersection of Two Arrays II

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -4352,6 +4352,45 @@ var arithmeticTriplets = function (nums, diff) {
 console.log(arithmeticTriplets([0, 1, 4, 6, 7, 10], 3)); // 2
 console.log(arithmeticTriplets([4, 5, 6, 7, 8, 9], 2)); // 2
 
+// * 2. (350) Intersection of Two Arrays II ----------------------------------------------------
+// Given two integer arrays nums1 and nums2, return an array of their intersection.
+// Each element in the result must appear as many times as it shows in both arrays
+// and you may return the result in any order.
+
+// Example 1:
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2,2]
+
+// Example 2:
+// Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// Output: [4,9]
+// Explanation: [9,4] is also accepted.
+
+const intersect = function (nums1, nums2) {
+  let result = [];
+  let sortedNums1 = nums1.sort((a, b) => a - b);
+  let sortedNums2 = nums2.sort((a, b) => a - b);
+  let i = 0;
+  let j = 0;
+
+  while (i < sortedNums1.length && j < sortedNums2.length) {
+    if (sortedNums1[i] === sortedNums2[j]) {
+      result.push(sortedNums1[i]);
+      i++;
+      j++;
+    } else if (sortedNums1[i] < sortedNums2[j]) {
+      i++;
+    } else {
+      j++;
+    }
+  }
+
+  return result;
+};
+
+console.log(intersect([1, 2, 2, 1], [2, 2])); // [2,2]
+console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4])); // [4,9]
+
 // ! =============== Sorting ==============
 
 // * 1. Find Target Indices after Sorting Array ---------------------------------
