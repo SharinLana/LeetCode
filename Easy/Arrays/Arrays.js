09860d7e7bf1ea6dca46474f7c09b84b0f07e86b
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-09-05 23:31:06 -0700

    solved a Leetcode problem #108. Convert Sorted Array to Binary Search Tree

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -4979,6 +4979,40 @@ MyHashSet.prototype.contains = function (key) {
 
 // ! =============== Divide and Conquer ==============
 
+// * 1. (108). Convert Sorted Array to Binary Search Tree ------------------------------------------
+// Given an integer array nums where the elements are sorted in ascending order,
+// convert it to a height-balanced binary search tree.
+
+// Example 1:
+// Input: nums = [-10,-3,0,5,9]
+// Output: [0,-3,9,-10,null,5]
+// Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+// Example 2:
+// Input: nums = [1,3]
+// Output: [3,1]
+// Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+
+//  * Definition for a binary tree node.
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+const sortedArrayToBST = function (nums, left = 0, right = nums.length - 1) {
+  if (left > right) return null;
+  const mid = Math.floor((left + right) / 2);
+  return new TreeNode(
+    nums[mid],
+    sortedArrayToBST(nums, left, mid - 1), // here, right will eventually become < left
+    sortedArrayToBST(nums, mid + 1, right) //
+  );
+};
+
+console.log(sortedArrayToBST([-10, -3, 0, 5, 9])); //  [0,-3,9,-10,null,5] or [0,-10,5,null,-3,null,9]
+console.log(sortedArrayToBST([1, 3])); // [3,1] or [3,1], or [1,null,3]
+
 // ! =============== Trees ==============
 
 // ! =============== Linked List ==============
