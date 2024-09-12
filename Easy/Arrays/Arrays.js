086e7819ce9f2d09bfaa542c3d34f14a7fe93236
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-09-12 10:49:43 -0700

    solve a LeetCode problem #414. Third Maximum Number

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -4494,6 +4494,48 @@ console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)); // [1,2,2,3,5,6]
 console.log(merge([1], 1, [], 0)); // [1]
 console.log(merge([0], 0, [1], 1)); // [1]
 
+// * 4. (88). Third Maximum Number ---------------------------------
+// Given an integer array nums, return the third distinct maximum number in this array. 
+// If the third maximum does not exist, return the maximum number.
+
+// Example 1:
+// Input: nums = [3,2,1]
+// Output: 1
+// Explanation:
+// The first distinct maximum is 3.
+// The second distinct maximum is 2.
+// The third distinct maximum is 1.
+
+// Example 2:
+// Input: nums = [1,2]
+// Output: 2
+// Explanation:
+// The first distinct maximum is 2.
+// The second distinct maximum is 1.
+// The third distinct maximum does not exist, so the maximum (2) is returned instead.
+
+// Example 3:
+// Input: nums = [2,2,3,1]
+// Output: 1
+// Explanation:
+// The first distinct maximum is 3.
+// The second distinct maximum is 2 (both 2's are counted together since they have the same value).
+// The third distinct maximum is 1.
+
+let thirdMax = function(nums) {
+    let sortedDistinctNums = Array.from(new Set(nums)).sort((a,b) => b - a);
+    
+    if (sortedDistinctNums[2] !== undefined) {
+      return sortedDistinctNums[2]
+    } else {
+      return Math.max.apply(null, sortedDistinctNums);
+    }
+};
+
+console.log(thirdMax([3, 2, 1])); // 1
+console.log(thirdMax([1, 2])); // 2
+console.log(thirdMax([2, 2, 3, 1])); // 1
+
 // ! =============== Prefix Sum ==============
 
 // * 1. Running Sum of 1d Array ---------------------------------
