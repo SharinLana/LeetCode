8e9f9135f08b4f07ae0665501b1def75e1335d2f
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-09-17 18:37:08 -0700

    solve a LeetCode problem #495. Teemo Attacking

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -2126,6 +2126,56 @@ console.log(lastVisitedIntegers([1, -1, 2, -1, -1])); // [1,2,1]
 console.log(lastVisitedIntegers([1, 2, -1, -1, -1])); // [2,1,-1]
 console.log(lastVisitedIntegers([-1, -1, -1, 52, -1])); // [-1,-1,-1,52]
 
+// * 10 (495). Teemo Attacking ----------------------------------------------------
+// Our hero Teemo is attacking an enemy Ashe with poison attacks!
+// When Teemo attacks Ashe, Ashe gets poisoned for a exactly duration seconds.
+// More formally, an attack at second t will mean Ashe is poisoned
+// during the inclusive time interval [t, t + duration - 1].
+// If Teemo attacks again before the poison effect ends,
+// the timer for it is reset, and the poison effect will end duration seconds
+// after the new attack.
+
+// You are given a non-decreasing integer array timeSeries,
+// where timeSeries[i] denotes that Teemo attacks Ashe at second timeSeries[i],
+// and an integer duration.
+
+// Return the total number of seconds that Ashe is poisoned.
+
+// Example 1:
+// Input: timeSeries = [1,4], duration = 2
+// Output: 4
+// Explanation: Teemo's attacks on Ashe go as follows:
+// - At second 1, Teemo attacks, and Ashe is poisoned for seconds 1 and 2.
+// - At second 4, Teemo attacks, and Ashe is poisoned for seconds 4 and 5.
+// Ashe is poisoned for seconds 1, 2, 4, and 5, which is 4 seconds in total.
+
+// Example 2:
+// Input: timeSeries = [1,2], duration = 2
+// Output: 3
+// Explanation: Teemo's attacks on Ashe go as follows:
+// - At second 1, Teemo attacks, and Ashe is poisoned for seconds 1 and 2.
+// - At second 2 however, Teemo attacks again and resets the poison timer.
+// Ashe is poisoned for seconds 2 and 3.
+// Ashe is poisoned for seconds 1, 2, and 3, which is 3 seconds in total.
+
+const findPoisonedDuration = function (timeSeries, duration) {
+  let counter = duration;
+
+  for (let i = 1; i < timeSeries.length; i++) {
+    if (timeSeries[i] > timeSeries[i - 1] + (duration - 1)) {
+      counter += duration;
+    } else {
+      counter += timeSeries[i] - timeSeries[i - 1];
+    }
+  }
+
+  return counter;
+};
+
+console.log(findPoisonedDuration([1, 4], 2)); // 4
+console.log(findPoisonedDuration([1, 2], 2)); // 3
+console.log(findPoisonedDuration([1, 2, 3, 4, 5, 6, 7, 8, 9], 5)); // 13
+
 // ! =============== Math / Counting Problems ===================!==undefinedundefined
 
 // * 1. Number of Good Pairs ----------------------------------------------------
@@ -4395,7 +4445,7 @@ const islandPerimeter = function (grid) {
         // Counting side segments
         if (j === 0) counter++;
         if (j === grid[i].length - 1) counter++;
-        
+
         // Counting top and bottom segments that connected to 0's
         if (i - 1 !== -1 && grid[i - 1][j] === 0) counter++;
         if (i + 1 !== grid.length && grid[i + 1][j] === 0) counter++;
