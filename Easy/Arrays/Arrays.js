b936f648770f4c138e74f8c5b37e21fac5fea85a
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-09-17 20:04:29 -0700

    solve a LeetCode problem #496. Next Greater Element I

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -3760,6 +3760,63 @@ let majorityElement = function (nums) {
 console.log(majorityElement([3, 2, 3])); // 3
 console.log(majorityElement([2, 2, 1, 1, 1, 2, 2])); // 2
 
+// * 15 (496). Next Greater Element I ------------------------------------------
+// The next greater element of some element x in an array is the first greater element
+// that is to the right of x in the same array.
+// You are given two distinct 0-indexed integer arrays nums1 and nums2,
+// where nums1 is a subset of nums2.
+// For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j]
+// and determine the next greater element of nums2[j] in nums2. If there is no next greater element,
+// then the answer for this query is -1.
+// Return an array ans of length nums1.length such that ans[i] is the next greater element
+// as described above.
+
+// Example 1:
+// Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+// Output: [-1,3,-1]
+// Explanation: The next greater element for each value of nums1 is as follows:
+// - 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+// - 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+// - 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+
+// Example 2:
+// Input: nums1 = [2,4], nums2 = [1,2,3,4]
+// Output: [3,-1]
+// Explanation: The next greater element for each value of nums1 is as follows:
+// - 2 is underlined in nums2 = [1,2,3,4]. The next greater element is 3.
+// - 4 is underlined in nums2 = [1,2,3,4]. There is no next greater element, so the answer is -1.
+const nextGreaterElement = function (nums1, nums2) {
+  let hashTable = {};
+  let i = 0;
+  let result = [];
+
+  while (i < nums2.length - 1) {
+    let current = nums2[i];
+    // Save each element in the hashTable with a value of the next greater element
+    for (let j = i + 1; j < nums2.length; j++) {
+      if (nums2[j] > current) {
+        hashTable[current] = nums2[j];
+        break;
+      } else {
+        continue;
+      }
+    }
+    i++;
+  }
+
+  for (let elem of nums1) {
+    if (hashTable[elem]) {
+      result.push(hashTable[elem]);
+    } else {
+      result.push(-1);
+    }
+  }
+
+  return result;
+};
+console.log(nextGreaterElement([4, 1, 2], [1, 3, 4, 2])); // [-1,3,-1]
+console.log(nextGreaterElement([2, 4], [1, 2, 3, 4])); // [3,-1]
+
 // ! =================== Matrix ====================
 
 // * 1. Matrix Cells in Distance Order ------------------------------------------
@@ -5204,7 +5261,7 @@ const maxProfit = function (prices) {
     if (prices[i] < minPrice) {
       minPrice = prices[i];
     } else {
-      maxDiff = Math.max(maxDiff, (prices[i] - minPrice))
+      maxDiff = Math.max(maxDiff, prices[i] - minPrice);
     }
   }
 
