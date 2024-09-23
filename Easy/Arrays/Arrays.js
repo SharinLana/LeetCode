63f7b034606cc6467d1a6801be9da2b333ba926e
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-09-22 21:45:07 -0700

    solve a Leetcode problem #605. Can Place Flowers

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -4559,11 +4559,11 @@ var maxCount = function (m, n, ops) {
   if (!ops.length) {
     return m * n;
   } else {
-    ops.map(arr => {
+    ops.map((arr) => {
       smallestX = Math.min(smallestX, arr[0]);
       smallestY = Math.min(smallestY, arr[1]);
-    })
-      
+    });
+
     return smallestX * smallestY;
   }
 };
@@ -4904,6 +4904,48 @@ const arrayPairSum = function (nums) {
 
 console.log(arrayPairSum([1, 4, 3, 2])); // 4
 console.log(arrayPairSum([6, 2, 6, 5, 1, 2])); // 9
+
+// * 7. (605). Can Place Flowers ---------------------------------
+// You have a long flowerbed in which some of the plots are planted, and some are not.
+// However, flowers cannot be planted in adjacent plots.
+
+// Given an integer array flowerbed containing 0's and 1's,
+// where 0 means empty and 1 means not empty, and an integer n,
+// return true if n new flowers can be planted in the flowerbed
+// without violating the no-adjacent-flowers rule and false otherwise.
+
+// Example 1:
+// Input: flowerbed = [1,0,0,0,1], n = 1
+// Output: true
+
+// Example 2:
+// Input: flowerbed = [1,0,0,0,1], n = 2
+// Output: false
+
+const canPlaceFlowers = function (flowerbed, n) {
+  let counter = n;
+
+  for (let i = 0; i < flowerbed.length; i++) {
+    if (
+      flowerbed[i] === 0 &&
+      (flowerbed[i - 1] === 0 || flowerbed[i - 1] === undefined) &&
+      (flowerbed[i + 1] === 0 || flowerbed[i + 1] === undefined) &&
+      counter > 0
+    ) {
+      counter--;
+      flowerbed[i] = 1;
+    }
+
+    if (counter === 0) return true;
+  }
+
+  return false;
+};
+
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 1)); // true
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 2)); // false
+console.log(canPlaceFlowers([1, 0, 1, 0, 1, 0, 1], 1)); // false
+console.log(canPlaceFlowers([0, 0, 1, 0, 0], 2)); // true
 
 // ! =============== Prefix Sum ==============
 
