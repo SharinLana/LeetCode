25e05e19b765bb886370a169b448d8036b9f1f84
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-09-30 19:10:19 -0700

    solve a LeetCode problem #724. Find Pivot Index

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -1807,14 +1807,14 @@ const isOneBitCharacter = function (bits) {
   }
 
   for (let i = 0; i < bits.length; i++) {
-    // if there's 1, then cut the segment from the beginning of the array to i+1 
+    // if there's 1, then cut the segment from the beginning of the array to i+1
     // and start checking the rest of the array for 1's
     if (bits[i] === 1) {
       return isOneBitCharacter(bits.slice(i + 2));
     } else {
       // if only 0's are in the array
       if (!bits.includes(1)) {
-        return true
+        return true;
       }
     }
   }
@@ -5353,6 +5353,66 @@ var largestAltitude = function (gain) {
 
 console.log(largestAltitude([-5, 1, 5, 0, -7])); // 1
 console.log(largestAltitude([-4, -3, -2, -1, 4, 3, 2])); // 0
+
+// * 4. 724. Find Pivot Index ---------------------------------
+// Given an array of integers nums, calculate the pivot index of this array.
+// The pivot index is the index where the sum of all the numbers strictly to the left
+// of the index is equal to the sum of all the numbers strictly to the index's right.
+// If the index is on the left edge of the array, then the left sum is 0
+// because there are no elements to the left. This also applies to the right edge of the array.
+
+// Return the leftmost pivot index. If no such index exists, return -1.
+
+// Example 1:
+// Input: nums = [1,7,3,6,5,6]
+// Output: 3
+// Explanation:
+// The pivot index is 3.
+// Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+// Right sum = nums[4] + nums[5] = 5 + 6 = 11
+
+// Example 2:
+// Input: nums = [1,2,3]
+// Output: -1
+// Explanation:
+// There is no index that satisfies the conditions in the problem statement.
+
+// Example 3:
+// Input: nums = [2,1,-1]
+// Output: 0
+// Explanation:
+// The pivot index is 0.
+// Left sum = 0 (no elements to the left of index 0)
+// Right sum = nums[1] + nums[2] = 1 + -1 = 0
+
+const pivotIndex = function (nums) {
+  let leftSum, rightSum;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0) {
+      leftSum = nums.slice(0, i).reduce((acc, el) => acc + el, 0);
+    } else {
+      leftSum = 0;
+    }
+
+    if (i < nums.length - 1) {
+      rightSum = nums.slice(i + 1).reduce((acc, el) => acc + el, 0);
+    } else {
+      rightSum = 0;
+    }
+
+    if (leftSum === rightSum) {
+      return i;
+    }
+  }
+
+  return -1;
+};
+
+console.log(pivotIndex([1, 7, 3, 6, 5, 6])); // 3
+console.log(pivotIndex([1, 2, 3])); // -1
+console.log(pivotIndex([2, 1, -1])); // 0
+console.log(pivotIndex([-1, -1, 0, 0, -1, -1])); // 2
 
 // ! =============== Bit Manipulation ==============
 
