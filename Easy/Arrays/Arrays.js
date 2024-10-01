a2573a3f9ce1dca7a434bb2dc66bd5ea8cde6054
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-10-01 16:34:57 -0700

    solve a LeetCode problem #744. Find Smallest Letter Greater Than Target

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -1826,6 +1826,46 @@ console.log(isOneBitCharacter([0, 0])); // true
 console.log(isOneBitCharacter([1, 0])); // false
 console.log(isOneBitCharacter([1, 0, 1, 0, 0])); // true
 
+// * 45. (744) Find Smallest Letter Greater Than Target ----------------------------------------------------
+// You are given an array of characters letters that is sorted in non-decreasing order, 
+// and a character target. 
+// There are at least two different characters in letters.
+
+// Return the smallest character in letters that is lexicographically greater than target. 
+// If such a character does not exist, return the first character in letters.
+
+// Example 1:
+// Input: letters = ["c","f","j"], target = "a"
+// Output: "c"
+// Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
+
+// Example 2:
+// Input: letters = ["c","f","j"], target = "c"
+// Output: "f"
+// Explanation: The smallest character that is lexicographically greater than 'c' in letters is 'f'.
+
+// Example 3:
+// Input: letters = ["x","x","y","y"], target = "z"
+// Output: "x"
+// Explanation: There are no characters in letters that is lexicographically greater than 'z' 
+// so we return letters[0].
+
+const nextGreatestLetter = function(letters, target) {
+    let set = Array.from(new Set(letters));
+
+    if ((set[set.length - 1] <= target)) {
+      return set.shift();
+    } else {
+      for (let elem of set) {
+        if (elem > target) return elem;
+      }
+    }
+};
+
+console.log(nextGreatestLetter(["c","f","j"], "a")); // "c"
+console.log(nextGreatestLetter(["c","f","j"], "c")); // "f"
+console.log(nextGreatestLetter(["x","x","y","y"], "z")); // "x" 
+
 // ! =============== Simulation Problems ===================
 
 // * 1. Build Array From Permutation -----------------------------------------
