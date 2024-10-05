06c47cfcb2fee77feff3584471cf638b54cdffc1
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-10-04 20:40:20 -0700

    solve a LeetCode problem #746. Min Cost Climbing Stairs

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -6022,6 +6022,46 @@ const maxProfit = function (prices) {
 console.log(maxProfit([7, 1, 5, 3, 6, 4])); // 5
 console.log(maxProfit([7, 6, 4, 3, 1])); // 0
 
+// * 4. (746) Min Cost Climbing Stairs -----------------------------------
+// You are given an integer array cost where cost[i] is the cost of ith step on a staircase. 
+// Once you pay the cost, you can either climb one or two steps.
+
+// You can either start from the step with index 0, or the step with index 1.
+
+// Return the minimum cost to reach the top of the floor.
+
+// Example 1:
+// Input: cost = [10,15,20]
+// Output: 15
+// Explanation: You will start at index 1.
+// - Pay 15 and climb two steps to reach the top.
+// The total cost is 15.
+
+// Example 2:
+// Input: cost = [1,100,1,1,1,100,1,1,100,1]
+// Output: 6
+// Explanation: You will start at index 0.
+// - Pay 1 and climb two steps to reach index 2.
+// - Pay 1 and climb two steps to reach index 4.
+// - Pay 1 and climb two steps to reach index 6.
+// - Pay 1 and climb one step to reach index 7.
+// - Pay 1 and climb two steps to reach index 9.
+// - Pay 1 and climb one step to reach the top.
+// The total cost is 6.
+
+const minCostClimbingStairs = function(cost) {
+    // start looping from the 3re element to the end go to the beginning
+    // change the value of each element by summing it up 
+    // with the lowest cost[i+1], cost [i+2] element 
+    for (let i = cost.length - 3; i >= 0; i--) {
+      cost[i] += Math.min(cost[i+1], cost[i+2]);
+    }
+    return Math.min(cost[0], cost[1]);
+};
+
+console.log(minCostClimbingStairs([10,15,20])); // 15
+console.log(minCostClimbingStairs([1,100,1,1,1,100,1,1,100,1])); // 6
+
 // ! =============== Design ==============
 
 // * 1. (303). Range Sum Query - Immutable -----------------------------------
