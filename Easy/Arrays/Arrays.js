8cfa61ec29d910b060af2cd6f362f815480785d5
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-10-06 18:05:31 -0700

    solve a LeetCode problem #704. Binary Search

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -5989,7 +5989,44 @@ const getMin = (arr) => {
 console.log(numberGame([5, 4, 2, 3])); // [3,2,5,4]
 console.log(numberGame([2, 5])); // [5,2]
 
-// ! =============== Search (Binary) ==============
+// ! =============== Binary Search ==============
+// * 1. (704) Binary Search -----------------------------------
+// Given an array of integers nums which is sorted in ascending order,
+// and an integer target, write a function to search target in nums.
+// If target exists, then return its index. Otherwise, return -1.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Example 1:
+// Input: nums = [-1,0,3,5,9,12], target = 9
+// Output: 4
+// Explanation: 9 exists in nums and its index is 4
+
+// Example 2:
+// Input: nums = [-1,0,3,5,9,12], target = 2
+// Output: -1
+// Explanation: 2 does not exist in nums so return -1
+
+var search = function (nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+  return -1;
+};
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9)); // 4
+console.log(search([-1, 0, 3, 5, 9, 12], 4)); // -1
+console.log(search([5], 5)); // 0
 
 // ! =============== Stack ==============
 
