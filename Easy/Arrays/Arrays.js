5fe6886d451d9b9070c655f8766d4ef30765c794
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-10-09 15:59:12 -0700

    solve a LeetCode problem #821. Shortest Distance to a Character

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -5343,6 +5343,68 @@ const intersect = function (nums1, nums2) {
 console.log(intersect([1, 2, 2, 1], [2, 2])); // [2,2]
 console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4])); // [4,9]
 
+// * 3. (821). Shortest Distance to a Character ----------------------------------------------------
+// Given a string s and a character c that occurs in s,
+// return an array of integers answer where answer.length == s.length
+// and answer[i] is the distance from index i to the closest occurrence of character c in s.
+
+// The distance between two indices i and j is abs(i - j), where abs is the absolute value function.
+
+// Example 1:
+// Input: s = "loveleetcode", c = "e"
+// Output: [3,2,1,0,1,0,0,1,2,2,1,0]
+// Explanation: The character 'e' appears at indices 3, 5, 6, and 11 (0-indexed).
+// The closest occurrence of 'e' for index 0 is at index 3, so the distance is abs(0 - 3) = 3.
+// The closest occurrence of 'e' for index 1 is at index 3, so the distance is abs(1 - 3) = 2.
+// For index 4, there is a tie between the 'e' at index 3 and the 'e' at index 5, but the distance is still the same: abs(4 - 3) == abs(4 - 5) = 1.
+// The closest occurrence of 'e' for index 8 is at index 6, so the distance is abs(8 - 6) = 2.
+
+// Example 2:
+// Input: s = "aaab", c = "b"
+// Output: [3,2,1,0]
+
+const shortestToChar = function (s, c) {
+  let result = [];
+  let start = 0;
+  let prevEnd;
+  let end = 0;
+
+  while (start < s.length) {
+    if (end < s.length) {
+      if (s[end] !== c) {
+        end++;
+      } else {
+        while (start !== end) {
+          if (
+            prevEnd !== undefined &&
+            Math.abs(prevEnd - start) < end - start
+          ) {
+            result.push(Math.abs(prevEnd - start));
+          } else {
+            result.push(end - start);
+          }
+          start++;
+        }
+        prevEnd = end;
+        result.push(0);
+        start++;
+        end++;
+      }
+    } else {
+      if (start <= s.length && prevEnd !== undefined) {
+        result.push(Math.abs(prevEnd - start));
+        start++;
+      }
+    }
+  }
+
+  return result;
+};
+
+console.log(shortestToChar("loveleetcode", "e")); // [3,2,1,0,1,0,0,1,2,2,1,0]
+console.log(shortestToChar("aaab", "b")); // [3,2,1,0]
+console.log(shortestToChar("aaba", "b")); // [2,1,0,1]
+
 // ! =============== Sorting ==============
 
 // * 1. Find Target Indices after Sorting Array ---------------------------------
