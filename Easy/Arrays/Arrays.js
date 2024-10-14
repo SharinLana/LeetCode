9dbac29022af422e2f3fb090b7c3cb0bd658ab83
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-10-13 23:14:39 -0700

    solve a LeetCode problem #883. Projection Area of 3D Shapes

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -5318,6 +5318,85 @@ console.log(
   ])
 ); // [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
 
+// * 15. (883).  Projection Area of 3D Shapes ------------------------------------------
+// You are given an n x n grid where we place some 1 x 1 x 1 cubes
+// that are axis-aligned with the x, y, and z axes.
+// Each value v = grid[i][j] represents a tower of v cubes
+// placed on top of the cell (i, j).
+// We view the projection of these cubes onto the xy, yz, and zx planes.
+// A projection is like a shadow, that maps our 3-dimensional figure
+// to a 2-dimensional plane.
+// We are viewing the "shadow" when looking at the cubes from the top,
+// the front, and the side.
+
+// Return the total area of all three projections.
+
+// Example 1:
+// Input: grid = [[1,2],[3,4]]
+// Output: 17
+// Explanation: Here are the three projections ("shadows") of the shape made with each axis-aligned plane.
+
+// Example 2:
+// Input: grid = [[2]]
+// Output: 5
+
+// Example 3:
+// Input: grid = [[1,0],[0,2]]
+// Output: 8
+
+var projectionArea = function (grid) {
+  // Area of the shadow (view from the top): count all the digits that are > than 0;
+  // Area of the shadow (front view): add the biggest values of each row
+  // Area of the shadow (side view): add the biggest values of each column
+
+  let topArea = grid.flat().filter((el) => el !== 0).length;
+  let frontArea = 0;
+  let sideArea = 0;
+  let i = 0;
+  let j = 0;
+  let biggestColValue = -Infinity;
+
+  // Calculating the biggest value of each row
+  for (let i = 0; i < grid.length; i++) {
+    frontArea += Math.max(...grid[i]);
+  }
+
+  // Calculating the biggest value of each column
+  while (j < grid[0].length) {
+    biggestColValue = Math.max(biggestColValue, grid[i][j]);
+    i++;
+
+    if (i > grid.length - 1) {
+      sideArea += biggestColValue;
+      i = 0;
+      j++;
+      biggestColValue = -Infinity;
+    }
+  }
+
+  return topArea + frontArea + sideArea;
+};
+
+console.log(
+  projectionArea([
+    [1, 2],
+    [3, 4],
+  ])
+); // 17
+console.log(projectionArea([[2]])); // 5
+console.log(
+  projectionArea([
+    [1, 0],
+    [0, 2],
+  ])
+); // 8
+console.log(
+  projectionArea([
+    [1, 4],
+    [1, 1],
+  ])
+); // 14
+
 // ! =============== Two Pointers ==============
 
 // * 1. Number of Arithmetic Triplets ------------------------------------------
