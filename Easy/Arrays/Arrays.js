325aacd7ad2c60de4234ccad3f187d7378154983
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-10-15 19:12:01 -0700

    solve a LeetCode problem #888 Fair Candy Swap

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -1866,6 +1866,57 @@ console.log(nextGreatestLetter(["c", "f", "j"], "a")); // "c"
 console.log(nextGreatestLetter(["c", "f", "j"], "c")); // "f"
 console.log(nextGreatestLetter(["x", "x", "y", "y"], "z")); // "x"
 
+// * 46. (888). Fair Candy Swap ------------------------------------------
+// Alice and Bob have a different total number of candies.
+// You are given two integer arrays aliceSizes and bobSizes
+// where aliceSizes[i] is the number of candies of the ith box of candy
+// that Alice has and bobSizes[j] is the number of candies of the jth box of candy that Bob has.
+
+// Since they are friends, they would like to exchange one candy box each
+// so that after the exchange, they both have the same total amount of candy.
+// The total amount of candy a person has is the sum of the number of candies in each box they have.
+
+// Return an integer array answer where answer[0] is the number of candies in the box
+// that Alice must exchange, and answer[1] is the number of candies in the box that Bob must exchange.
+// If there are multiple answers, you may return any one of them.
+// It is guaranteed that at least one answer exists.
+
+// Example 1:
+// Input: aliceSizes = [1,1], bobSizes = [2,2]
+// Output: [1,2]
+
+// Example 2:
+// Input: aliceSizes = [1,2], bobSizes = [2,3]
+// Output: [1,2]
+
+// Example 3:
+// Input: aliceSizes = [2], bobSizes = [1,3]
+// Output: [2,3]
+
+const fairCandySwap = function (aliceSizes, bobSizes) {
+  let fairShare = getCandySum(aliceSizes.concat(bobSizes)) / 2;
+  let aliceCandies = getCandySum(aliceSizes);
+
+  for (let i = 0; i < aliceSizes.length; i++) {
+    let numberToExchange = fairShare - (aliceCandies - aliceSizes[i]);
+
+    if (bobSizes.includes(numberToExchange)) {
+      return [aliceSizes[i], numberToExchange];
+    }
+  }
+
+  return [];
+};
+
+const getCandySum = (arr) => {
+  return arr.reduce((acc, cand) => acc + cand, 0);
+};
+
+console.log(fairCandySwap([1, 1], [2, 2])); // [1, 2]
+console.log(fairCandySwap([1, 2], [2, 3])); // [1, 2]
+console.log(fairCandySwap([2], [1, 3])); // [2, 3]
+console.log(fairCandySwap([1, 2, 5], [2, 4])); // [5, 4]
+
 // ! =============== Simulation Problems ===================
 
 // * 1. Build Array From Permutation -----------------------------------------
