d071bbc9a482e2229b585361efe9e8db9fd698d1
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-10-18 17:33:39 -0700

    solve a LeetCode problem #922. Sort Array By Parity II

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -6091,7 +6091,51 @@ const sortArrayByParity = function (nums) {
 };
 
 console.log(sortArrayByParity([3, 1, 2, 4])); // [2,4,3,1] (or [4,2,3,1], or [2,4,1,3], or [4,2,1,3])
-// console.log(sortArrayByParity([0])); // [0]
+console.log(sortArrayByParity([0])); // [0]
+
+// * 11. (922). Sort Array By Parity II ------------------------------------------
+// Given an array of integers nums, half of the integers in nums are odd,
+// and the other half are even.
+
+// Sort the array so that whenever nums[i] is odd, i is odd,
+// and whenever nums[i] is even, i is even.
+
+// Return any answer array that satisfies this condition.
+
+// Example 1:
+// Input: nums = [4,2,5,7]
+// Output: [4,5,2,7]
+// Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+
+// Example 2:
+// Input: nums = [2,3]
+// Output: [2,3]
+
+const sortArrayByParityII = function (nums) {
+  let oddNums = [];
+  let evenNums = [];
+
+  for (let elem of nums) {
+    if (elem % 2 === 0) {
+      evenNums.push(elem);
+    } else {
+      oddNums.push(elem);
+    }
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 2 === 0) {
+      nums[i] = evenNums.shift();
+    } else {
+      nums[i] = oddNums.shift();
+    }
+  }
+
+  return nums;
+};
+
+console.log(sortArrayByParityII([4, 2, 5, 7])); // [4,5,2,7]
+console.log(sortArrayByParityII([2, 3])); // [2, 3]
 
 // ! =============== Prefix Sum ==============
 
