880da3a06cc8eb02a13d4297b8e170cafc302fb2
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-10-19 20:20:28 -0700

    solve a LeetCode problem #944. Delete Columns to Make Sorted

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -3513,7 +3513,7 @@ const diStringMatch = function (s) {
   let dArr = [];
 
   for (let i = 0; i <= s.length; i++) {
-    if (i <= iCounter ) {
+    if (i <= iCounter) {
       iArr.push(i);
     } else {
       dArr.push(i);
@@ -3524,7 +3524,7 @@ const diStringMatch = function (s) {
 
   for (let elem of s) {
     if (elem === "I") {
-      result.push(iArr.shift())
+      result.push(iArr.shift());
     } else {
       result.push(dArr.pop());
     }
@@ -3537,6 +3537,52 @@ const diStringMatch = function (s) {
 console.log(diStringMatch("IDID")); // [0,4,1,3,2]
 console.log(diStringMatch("III")); // [0,1,2,3]
 console.log(diStringMatch("DDI")); // [3,2,0,1]
+
+// * 17. (944). Delete Columns to Make Sorted ------------------------------------------
+// You are given an array of n strings strs, all of the same length.
+// The strings can be arranged such that there is one on each line, making a grid.
+// For example, strs = ["abc", "bce", "cae"] can be arranged as follows:
+// abc
+// bce
+// cae
+// You want to delete the columns that are not sorted lexicographically.
+// In the above example (0-indexed), columns 0 ('a', 'b', 'c')
+// and 2 ('c', 'e', 'e') are sorted, while column 1 ('b', 'c', 'a') is not,
+// so you would delete column 1.
+// Return the number of columns that you will delete.
+
+// Example 1:
+// Input: strs = ["cba","daf","ghi"]
+// Output: 1
+// Explanation: The grid looks as follows:
+//   cba
+//   daf
+//   ghi
+// Columns 0 and 2 are sorted, but column 1 is not, so you only need to delete 1 column.
+const minDeletionSize = function (strs) {
+  let counter = 0;
+  let i = 0;
+  let j = 0;
+
+  while (strs[i + 1] !== undefined && j < strs[i].length) {
+    if (strs[i][j] > strs[i + 1][j]) {
+      counter++;
+      i = 0;
+      j++;
+    } else {
+      i++;
+      if (i >= strs.length - 1) {
+        i = 0;
+        j++;
+      }
+    }
+  }
+
+  return counter;
+};
+console.log(minDeletionSize(["cba", "daf", "ghi"])); // 1
+console.log(minDeletionSize(["a", "b"])); // 0
+console.log(minDeletionSize(["zyx", "wvu", "tsr"])); // 3
 
 // ! =============== Enumeration ===================
 
@@ -7128,3 +7174,4 @@ console.log(sortedArrayToBST([1, 3])); // [3,1] or [3,1], or [1,null,3]
 // ! =============== Linked List ==============
 
 // ! =============== DFS, BFS ==============
+
