2ab118dac7e9908b71c16c1a74a980c32e620f3f
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-10-21 17:28:03 -0700

    solve a LeetCode problem #961. N-Repeated Element in Size 2N Array

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -4581,6 +4581,43 @@ console.log(hasGroupsSizeX([1, 1, 1, 2, 2, 2, 3, 3])); // false
 console.log(hasGroupsSizeX([1])); // false
 console.log(hasGroupsSizeX([1, 1, 1, 1, 2, 2, 2, 2, 2, 2])); // true
 
+// * 19. (961). N-Repeated Element in Size 2N Array ------------------------------------------
+// You are given an integer array nums with the following properties:
+// nums.length == 2 * n.
+// nums contains n + 1 unique elements.
+// Exactly one element of nums is repeated n times.
+// Return the element that is repeated n times.
+
+// Example 1:
+// Input: nums = [1,2,3,3]
+// Output: 3
+
+// Example 2:
+// Input: nums = [2,1,2,5,3,2]
+// Output: 2
+
+// Example 3:
+// Input: nums = [5,1,5,2,5,3,5,4]
+// Output: 5
+
+const repeatedNTimes = function (nums) {
+  let hashTbl = {};
+
+  for (let elem of nums) {
+    hashTbl[elem] ? (hashTbl[elem] += 1) : (hashTbl[elem] = 1);
+  }
+
+  const result = Object.keys(hashTbl).filter(
+    (key) => hashTbl[key] === nums.length / 2
+  );
+
+  return +result[0];
+};
+
+console.log(repeatedNTimes([1, 2, 3, 3])); // 3
+console.log(repeatedNTimes([2, 1, 2, 5, 3, 2])); // 2
+console.log(repeatedNTimes([5, 1, 5, 2, 5, 3, 5, 4])); // 5
+
 // ! =================== Matrix ====================
 
 // * 1. Matrix Cells in Distance Order ------------------------------------------
