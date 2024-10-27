0787a77c842ef697681bf0f5aca41b0753d4722d
Lana Sharin <lana.sharin.webdev@gmail.com>
2024-10-26 18:03:18 -0700

    solve a LeetCode problem #1013. Partition Array Into Three Parts With Equal Sum

--- Easy/Arrays/Arrays.js
+++ Easy/Arrays/Arrays.js
@@ -7061,6 +7061,59 @@ console.log(largestSumAfterKNegations([-2, 5, 0, 2, -2], 3)); // 11
 console.log(largestSumAfterKNegations([8, -7, -3, -9, 1, 9, -6, -9, 3], 8)); // 53
 console.log(largestSumAfterKNegations([-4, -2, -3], 4)); // 5
 
+// * 6. (1013). Partition Array Into Three Parts With Equal Sum ------------------------------------------
+// Given an array of integers arr, return true if we can partition the array
+// into three non-empty parts with equal sums.
+
+// Formally, we can partition the array if we can find indexes
+// i + 1 < j with (arr[0] + arr[1] + ... + arr[i] == arr[i + 1] + arr[i + 2] + ... + arr[j - 1] == arr[j] + arr[j + 1] + ... + arr[arr.length - 1])
+
+// Example 1:
+// Input: arr = [0,2,1,-6,6,-7,9,1,2,0,1]
+// Output: true
+// Explanation: 0 + 2 + 1 = -6 + 6 - 7 + 9 + 1 = 2 + 0 + 1
+
+// Example 2:
+// Input: arr = [0,2,1,-6,6,7,9,-1,2,0,1]
+// Output: false
+
+// Example 3:
+// Input: arr = [3,3,6,5,-2,2,5,1,-9,4]
+// Output: true
+// Explanation: 3 + 3 = 6 = 5 - 2 + 2 + 5 + 1 - 9 + 4
+
+const canThreePartsEqualSum = function (arr) {
+  let totalSum = arr.reduce((acc, el) => acc + el, 0);
+  if (totalSum % 3 !== 0) return false;
+
+  let goal = Math.floor(totalSum / 3);
+  let sum = 0;
+  let parts = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    if (sum === goal) {
+      sum = 0;
+      parts++;
+    }
+
+    if (parts === 3) return true;
+  }
+  return false;
+};
+
+console.log(canThreePartsEqualSum([0, 2, 1, -6, 6, -7, 9, 1, 2, 0, 1])); // true
+console.log(canThreePartsEqualSum([0, 2, 1, -6, 6, 7, 9, -1, 2, 0, 1])); // false
+console.log(canThreePartsEqualSum([3, 3, 6, 5, -2, 2, 5, 1, -9, 4])); // true
+console.log(canThreePartsEqualSum([18, 12, -18, 18, -19, -1, 10, 10])); // true
+console.log(canThreePartsEqualSum([12, -4, 16, -5, 9, -3, 3, 8, 0])); // true
+console.log(
+  canThreePartsEqualSum([10, -7, 13, -14, 30, 16, -3, -16, -27, 27, 19])
+); // true
+console.log(canThreePartsEqualSum([1, -1, 1, -1])); // false
+console.log(canThreePartsEqualSum([0, 0, 0, 0])); // true
+console.log(canThreePartsEqualSum([10, -10, 10, -10, 10, -10, 10, -10])); // true
+
 
 // ! =============== Backtracking ==============
 
